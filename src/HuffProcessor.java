342c34e709a2519f16f1a1294c1eb2d322504474
Javan Jiang <jj252@duke.edu>
2018-12-05 22:28:12 -0500

    adding decompress text

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -61,12 +61,31 @@ public class HuffProcessor {
 	 *            Buffered bit stream writing to the output file.
 	 */
 	public void decompress(BitInputStream in, BitOutputStream out){
-
+		
+		int bits = in.readBits(BITS_PER_INT);
+		if (bits !=HUFF_TREE) {
+			throw new HuffException ("illegal header starts with "+bits);
+		}
+		
+		HuffNode root = readTreeHeader(in);
+		readCompressedBits(root, in, out);
+		/*
 		while (true){
 			int val = in.readBits(BITS_PER_WORD);
 			if (val == -1) break;
 			out.writeBits(BITS_PER_WORD, val);
 		}
+		*/
 		out.close();
+	}
+
+	private void readCompressedBits(HuffNode root, BitInputStream in, BitOutputStream out) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private HuffNode readTreeHeader(BitInputStream in) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
